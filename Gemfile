156e7e7256d3b5cac9a4375b04c97179c79e3617
Alexander Mancevice <alexander.mancevice@hey.com>
2020-08-06 11:00:11 -0400

    upgrade to ses v2 API

--- Gemfile
+++ Gemfile
@@ -3,9 +3,9 @@ source "https://rubygems.org/"
 gem "mail", "~> 2.7"
 
 group :development do
-  gem "aws-sdk-s3",  "~> 1"
-  gem "aws-sdk-ses", "~> 1"
-  gem "dotenv",      "~> 2.7"
-  gem "pry",         "~> 0.12"
-  gem "rumrunner",   "~> 0.5"
+  gem "aws-sdk-s3",    "~> 1.75"
+  gem "aws-sdk-sesv2", "~> 1.9"
+  gem "dotenv",        "~> 2.7"
+  gem "pry",           "~> 0.12"
+  gem "rumrunner",     "~> 0.5"
 end
--- lambda.rb
+++ lambda.rb
@@ -1,13 +1,13 @@
 require "json"
 
 require "aws-sdk-s3"
-require "aws-sdk-ses"
+require "aws-sdk-sesv2"
 require "mail"
 
 DESTINATIONS = ENV["DESTINATIONS"].to_s.split(/,/)
 
 S3  = Aws::S3::Client.new
-SES = Aws::SES::Client.new
+SES = Aws::SESV2::Client.new
 
 def each_message(event)
   puts "EVENT #{event.to_json}"
@@ -22,16 +22,15 @@ def handler(event:, context:nil)
     bucket = message.dig "receipt", "action", "bucketName"
     key    = message.dig "receipt", "action", "objectKey"
     object = S3.get_object bucket: bucket, key: key
-    data   = object.body.read
 
     # Massage message for SES
-    mail             = Mail.read_from_string data
+    mail             = Mail.read_from_string object.body.read
     mail.to          = DESTINATIONS
-    mail.reply_to    = mail[:from].value
+    mail.reply_to    = mail.from
     mail.from        = "Brutalismbot Help <no-reply@brutalismbot.com>"
     mail.return_path = "<no-reply@brutalismbot.com>"
 
     # Forward message to `DESTINATIONS`
-    SES.send_raw_email raw_message: {data: mail.to_s}
+    SES.send_email(content: {raw: {data: mail.to_s}})
   end
 end
